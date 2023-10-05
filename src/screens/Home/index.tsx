b1ba143cc4f79a136430d9dabb9a24ca15a32142
Raphael Lima <raphaeldesousalm@gmal.com>
2023-10-04 22:53:19 -0300

    add search movie

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -19,8 +19,15 @@ const Home = ()=> {
     }
 
     const [discoveryMovies, setDiscoveryMovies] = useState<Movie[]>([])
+    const [serachResultMovies, setSerachResultMovies] = useState<Movie[]>([])
     const [page, setPage] = useState(1)
     const [loading, setLoading] = useState(false)
+    const [noResult, setNoResult] = useState(false)
+    const [search, setSearch] = useState('')
+
+    useEffect(()=> {
+        loadData()
+    }, [])
 
     const loadData = async () => {
         setLoading(true)
@@ -34,23 +41,52 @@ const Home = ()=> {
         setLoading(false)
     }
 
-    useEffect(()=> {
-        loadData()
-    }, [])
+    const searchMovies = async (query: string) => {
+        setLoading(true)
+        const response = await api.get("/search/movie", {
+            params: {
+                query,
+            }
+        })
+
+        if(response.data.results.lenght === 0) {
+            setNoResult(true)
+        } else{
+            setSerachResultMovies(response.data.results)
+        }
+        setLoading(false)
+    }
+
+    const handleSearch = (text: string) => {
+        setSearch(text)
+        if(text.length > 2){
+            searchMovies(text)
+        } else {
+            setSerachResultMovies([])
+        }
+    }
+
+    const movieData = search.length > 2 ? serachResultMovies : discoveryMovies
 
     return (
         <View style={styles.container}>
             <View style={styles.header}>
                 <Text style={styles.headerText}>O que você assisti hoje?</Text>
                 <View style={styles.containerInput}>
-                    <TextInput placeholderTextColor="#fff" placeholder="Buscar" style={styles.input}/>
+                    <TextInput 
+                        placeholderTextColor="#fff" 
+                        placeholder="Buscar" 
+                        style={styles.input}
+                        onChangeText={handleSearch}
+                        value={search}
+                        />
                     <MagnifyingGlass color="#fff" weight="light" size={25}/>
                 </View>
             </View>
             <View>
 
                 <FlatList 
-                data={discoveryMovies}
+                data={movieData}
                 numColumns={3}
                 renderItem={(item) => <CardMovies data={item.item}/>}
                 showsVerticalScrollIndicator={false}
